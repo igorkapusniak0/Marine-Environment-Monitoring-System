c27dc88ac9b937a4339a60c798efde894ba2852b
Igor Kapusniak <114166214+igorkapusniak0@users.noreply.github.com>
2024-04-18 22:03:17 +0100

    Update temp.ino

--- temp.ino
+++ temp.ino
@@ -1,8 +1,9 @@
 #include "OneWire.h"
 #include "DallasTemperature.h"
-
+// Pin D2 for OneWire
 #define ONE_WIRE_BUS 2
-
+int sec = 1000;
+int min = 60 * sec;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
@@ -14,6 +15,8 @@ void setup() {
 void loop() {
   sensors.requestTemperatures();
   float tempC = sensors.getTempCByIndex(0);
+  // Outputs data on Arduino TX pin which is connected to PIN 16 on Pycom SiPy
   Serial.println(tempC, 2);
-  delay(1500);
+  // Change to adjust temperature read frequency
+  delay(15*sec);
 }
